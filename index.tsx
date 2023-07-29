bb205a5cc56cfd5e427b86bc89aa5f29d416e8e5
Soory Ranganathan <soory.ranga@hotmail.com>
2023-07-29 11:48:44 -0400

    fix app imports

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@
  */
 import 'react-native-reanimated';
 import {AppRegistry} from 'react-native';
-import App from './src/App';
+import {App} from '@app/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
